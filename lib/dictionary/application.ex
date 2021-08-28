170b9b049cc57091c22512cd0b2b903d4601c5b4
harsh.kedia <harsh.kedia@alcemy.tech>
2021-08-28 14:54:32 +0200

    using a supervisor

--- lib/dictionary/application.ex
+++ lib/dictionary/application.ex
@@ -2,6 +2,13 @@ defmodule Dictionary.Application do
   use Application
 
   def start(_type, _args) do
-    Dictionary.WordList.start_link()
+    children = [
+      %{
+        id: Dictionary.WordList,
+        start: {Dictionary.WordList, :start_link, []}
+      }
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
